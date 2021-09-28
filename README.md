60728b7355c984f6b7fa00332f71ed6143580fe9
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-09-28 17:22:42 +0300

    upd

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # goit-js-hw-11-timer
+
+// const targetDate = new Date(2021, 9, 9);
+// const clockDays = document.querySelector('[data-value="days"]');
+// const clockHours = document.querySelector('[data-value="hours"]');
+// const clockMins = document.querySelector('[data-value="mins"]');
+// const clockSecs = document.querySelector('[data-value="secs"]');
+// let intervalId;
+
+// intervalId = setInterval(function () {
+// const currentDate = Date.now();
+// const time = targetDate - currentDate;
+// const { days, hours, mins, secs } = getTimeComponents(time);
+// updateClockFace({ days, hours, mins, secs });
+// console.log(`${days}:${hours}:${mins}:${secs}`);
+// if (time === 0) {
+// clearInterval(intervalId);
+// }
+// }, 1000);
+
+// function pad(value) {
+// return String(value).padStart(2, '0');
+// }
+
+// function getTimeComponents(time) {
+// const days = pad(Math.floor(time / (1000 _ 60 _ 60 _ 24)));
+// const hours = pad(Math.floor((time % (1000 _ 60 _ 60 _ 24)) / (1000 _ 60 _ 60)));
+// const mins = pad(Math.floor((time % (1000 _ 60 _ 60)) / (1000 _ 60)));
+// const secs = pad(Math.floor((time % (1000 _ 60)) / 1000));
+// return { days, hours, mins, secs };
+// }
+
+// function updateClockFace({ days, hours, mins, secs }) {
+// clockDays.textContent = `${days}`;
+// clockHours.textContent = `${hours}`;
+// clockMins.textContent = `${mins}`;
+// clockSecs.textContent = `${secs}`;
+// }
--- app.js
+++ app.js
@@ -15,7 +15,6 @@ class CountdownTimer {
 
     #initFaceElements() {
         this.timerElement = document.querySelector(this.selector);
-        // check if selector is correct
         this.#clockDays = this.timerElement.querySelector('[data-value="days"]');
         this.#clockHours = this.timerElement.querySelector('[data-value="hours"]');
         this.#clockMins = this.timerElement.querySelector('[data-value="mins"]');
@@ -59,40 +58,3 @@ class CountdownTimer {
 
 let timer = new CountdownTimer( new Date(2021, 9, 9, 23, 23, 23), '#timer-1');
 timer.start();
-
-// const targetDate = new Date(2021, 9, 9);
-// const clockDays = document.querySelector('[data-value="days"]');
-// const clockHours = document.querySelector('[data-value="hours"]');
-// const clockMins = document.querySelector('[data-value="mins"]');
-// const clockSecs = document.querySelector('[data-value="secs"]');
-// let intervalId;
-
-// intervalId = setInterval(function () {
-//     const currentDate = Date.now();
-//     const time = targetDate - currentDate;
-//     const { days, hours, mins, secs } = getTimeComponents(time);
-//     updateClockFace({ days, hours, mins, secs });
-//     console.log(`${days}:${hours}:${mins}:${secs}`);
-//     if (time === 0) {
-//     clearInterval(intervalId);
-//  }
-// }, 1000);
-    
-// function pad(value) {
-//     return String(value).padStart(2, '0');
-// }
-    
-// function getTimeComponents(time) {
-//     const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-//     const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-//     const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-//     const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-//     return { days, hours, mins, secs };
-// }
-
-// function updateClockFace({ days, hours, mins, secs }) {
-//     clockDays.textContent = `${days}`;
-//     clockHours.textContent = `${hours}`;
-//     clockMins.textContent = `${mins}`;
-//     clockSecs.textContent = `${secs}`;
-//  }
